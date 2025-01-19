05f984311a07c3de071ef156be121e8039f32f21
Ayubxon <aubsattorov@gmail.com>
2025-01-19 23:19:59 +0500

    Refactor app.js and add index.js with commented out Boolean logic examples

--- app.js
+++ app.js
@@ -1,52 +1,101 @@
-// let a = 25;
-
-// if (a <= 99) {
-//   console.log("Togri");
-// } else {
-//   console.log("Xatolik");
-// }
-
-// let son = 567;
-
-// let a = (son - (son % 100)) / 100;
-// let b = ((son % 100) - (son % 10)) / 10;
-// let c = son % 10;
-
-// if (a < b && b < c) {
-//   console.log("raqamlar ketma ket");
-// } else {
-//   console.log("raqamlar ketma ket emas");
-// }
-
-//Array
-
-// let a = 10;
-// let b = 15;
-// let c = 20;
-
-// if (a) {
-// }
-
-function varScoping() {
-  var x = 1;
-
-  if (true) {
-    var x = 2;
-    console.log(x);
-  }
-
-  console.log(x);
-}
-
-// function letScoping() {
-//   let x = 1;
-
-//   if (true) {
-//     let x = 2;
-//     console.log(x);
-//   }
-
-//   console.log(x);
-// }
-
 // console.log(x);
+
+// Boolean1
+// const A = parseInt(prompt("A sonini kiriting:"));
+
+// if (A > 0) {
+//   console.log("A soni musbat.");
+// } else {
+//   console.log("A soni musbat emas.");
+// }
+
+// Boolean2
+// const A = parseInt(prompt("A sonini kiriting:"));
+
+// if (A % 2) {
+//   console.log("A soni toq son.");
+// } else {
+//   console.log("A soni toq son emas.");
+// }
+
+// Boolean3
+// let a = 5;
+// let b = 4;
+// if (a > 2 && b <= 3) {
+//   console.log("a va b butun son");
+// } else {
+//   console.log("butun son emas");
+// }
+
+// Boolean4
+// let A = parseInt(prompt("A ni kiriting:"));
+// let B = parseInt(prompt("B ni kiriting:"));
+// let C = parseInt(prompt("C ni kiriting:"));
+
+// if (A <= B && B <= C) {
+//   console.log("Musbat son");
+// } else {
+//   console.log("Musbat son emas");
+// }
+
+// Boolean5
+// let A = parseInt(prompt("A sonini kiriting: "));
+// let B = parseInt(prompt("B sonini kiriting: "));
+
+// if ((A % 2 === 0 && B % 2 === 0) || (A % 2 !== 0 && B % 2 !== 0)) {
+//   console.log("butun son");
+// } else {
+//   console.log("butun son emas");
+// }
+
+// Boolean6
+// let A = 5.5;
+// let B = 2.4;
+// let C = 2.2;
+
+// if (A > 0 || B > 0 || C > 0) {
+//   console.log("butun son");
+// } else {
+//   console.log("butun son emas");
+// }
+
+// Boolean7
+// let son = 542;
+
+// if (son > 99 && son < 999) {
+//   console.log("uchxonali son");
+// } else {
+//   console.log("Uchxonali son emas");
+// }
+
+// Boolean8
+// let a = 1;
+// let b = 2;
+// let c = 3;
+
+// let natija;
+
+// if (a === b || a === c || b === c) {
+//   console.log("Rost");
+// } else {
+//   console.log("Yolg'on");
+// }
+
+// Boolean1
+// let A = 8;
+
+// if (A % 2 === 0) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+// Boolean2
+// let A = 10;
+// let B = 5;
+
+// if (A > B) {
+//   console.log(true);
+// } else {
+//   console.log(false);
+// }
